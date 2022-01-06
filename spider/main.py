5b346e4b1f9c53c87e3b7e6e4c33dd0a5baa980f
KuangjuX <18630816527@163.com>
2022-01-06 22:54:19 +0800

    整理代码 & 添加珍爱网爬虫

--- spider/main.py
+++ spider/main.py
@@ -1,10 +1,44 @@
-from single_spider import JSpider
+from single_spider import JSpider, ZSpider
 
 def jsingle_main():
     url = 'https://search.jiayuan.com/v2/search_v2.php'
     spider = JSpider(url)
     for page in range(1, 100):
-        spider.run(page)
+        payload = [
+            ('sex', 'f'),
+            ('key', ''), 
+            ('stc', '23:1'),
+            ('sn', 'default'),
+            ('sv', '1'),
+            ('p', str(page)),
+            ('f', ''), 
+            ('listStyle', 'bigPhoto'),
+            ('pri_uid', '254091710'),
+            ('jsversion', 'v5')
+        ]
+        spider.run(page, payload)
+
+def zsingle_main():
+    url = "https://www.zhenai.com/api/search/getConditionData.do"
+    spider = ZSpider(url)
+    for page in range(1, 10):
+        payload = {
+            ('sex', '1'),
+            ('workCity', '-1'),
+            ('ageBegin', '-1'),
+            ('ageEnd', '-1'),
+            ('heightBegin', '-1'),
+            ('heightEnd', '-1'),
+            ('body', '-1'),
+            ('multiEducation', '-1'),
+            ('salaryBegin', '-1'),
+            ('salaryEnd', '-1'),
+            ('page', str(page)),
+            ('pageSize', '20'),
+            ('_', '1641479868103'),
+            ('ua', 'h5/1.0.0/1/0/0/0/901045/0//0/0/97b254c7-1906-4f00-82f0-53adb10d15ba/0/0/83518767')
+        }
+        spider.run(page, payload)
 
 if __name__ == '__main__':
-    jsingle_main()
+    zsingle_main()
--- spider/single_spider.py
+++ spider/single_spider.py
@@ -16,31 +16,34 @@ class SingleSpider:
         self.session = requests.Session()
         self.headers = self.__get_user_agent()
 
+    def __del__(self):
+        print('[Debug] Spider finish and destory')
+
     # 获取随机 User-Agent 头，防止被识别为爬虫
     def __get_user_agent(self):
         user_agent_list = [
-        'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.8) Gecko Fedora/1.9.0.8-1.fc10 Kazehakase/0.5.6',
-        'Mozilla/5.0 (X11; Linux i686; U;) Gecko/20070322 Kazehakase/0.4.5',
-        'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.71 Safari/537.1 LBBROWSER',
-        'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)',
-        'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11',
-        'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
-        'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; QQDownload 732; .NET4.0C; .NET4.0E)',
-        'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11',
-        'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; SV1; QQDownload 732; .NET4.0C; .NET4.0E; 360SE)',
-        'Mozilla/4.0 (compatible; MSIE 7.0b; Windows NT 5.2; .NET CLR 1.1.4322; .NET CLR 2.0.50727; InfoPath.2; .NET CLR 3.0.04506.30)',
-        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/535.20 (KHTML, like Gecko) Chrome/19.0.1036.7 Safari/535.20',
-        'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.8) Gecko Fedora/1.9.0.8-1.fc10 Kazehakase/0.5.6',
-        'Mozilla/5.0 (X11; U; Linux x86_64; zh-CN; rv:1.9.2.10) Gecko/20100922 Ubuntu/10.10 (maverick) Firefox/3.6.10',
-        'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.71 Safari/537.1 LBBROWSER',
-        'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.89 Safari/537.1',
-        'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; Acoo Browser; SLCC1; .NET CLR 2.0.50727; Media Center PC 5.0; .NET CLR 3.0.04506)',
-        'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.0.12) Gecko/20070731 Ubuntu/dapper-security Firefox/1.5.0.12',
-        'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; QQDownload 732; .NET4.0C; .NET4.0E; LBBROWSER)',
-        'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.89 Safari/537.1',
-        'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.3 Mobile/14E277 Safari/603.1.30',
-        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
-    ]
+            'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.8) Gecko Fedora/1.9.0.8-1.fc10 Kazehakase/0.5.6',
+            'Mozilla/5.0 (X11; Linux i686; U;) Gecko/20070322 Kazehakase/0.4.5',
+            'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.71 Safari/537.1 LBBROWSER',
+            'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)',
+            'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11',
+            'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+            'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; QQDownload 732; .NET4.0C; .NET4.0E)',
+            'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11',
+            'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; SV1; QQDownload 732; .NET4.0C; .NET4.0E; 360SE)',
+            'Mozilla/4.0 (compatible; MSIE 7.0b; Windows NT 5.2; .NET CLR 1.1.4322; .NET CLR 2.0.50727; InfoPath.2; .NET CLR 3.0.04506.30)',
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/535.20 (KHTML, like Gecko) Chrome/19.0.1036.7 Safari/535.20',
+            'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.8) Gecko Fedora/1.9.0.8-1.fc10 Kazehakase/0.5.6',
+            'Mozilla/5.0 (X11; U; Linux x86_64; zh-CN; rv:1.9.2.10) Gecko/20100922 Ubuntu/10.10 (maverick) Firefox/3.6.10',
+            'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.71 Safari/537.1 LBBROWSER',
+            'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.89 Safari/537.1',
+            'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; Acoo Browser; SLCC1; .NET CLR 2.0.50727; Media Center PC 5.0; .NET CLR 3.0.04506)',
+            'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.0.12) Gecko/20070731 Ubuntu/dapper-security Firefox/1.5.0.12',
+            'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; QQDownload 732; .NET4.0C; .NET4.0E; LBBROWSER)',
+            'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.89 Safari/537.1',
+            'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.3 Mobile/14E277 Safari/603.1.30',
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
+        ]
 
     # 获取代理 ip 地址
     def __get_proxy(self):
@@ -51,13 +54,19 @@ class SingleSpider:
             print("[Exception]")
             sys.exit(-1)
 
+    def req(self, payload):
+        response = self.session.post(url = self.url, data=payload)
+        if response.status_code == 200:
+            return response.text
+        else:
+            print('[Error] Fail to request url')
+            return None
+
+    def run(self, page: int):
+        print('[Debug] Spider will work on page {}'.format(page))
+
 # 世纪佳缘爬虫的具体类
 class JSpider(SingleSpider):
-    def __init__(self, url):
-        # 调用父类的初始化方法
-        super(JSpider, self).__init__(url)
-
-
     def __deep_search(self, id):
         url = "https://www.jiayuan.com/{}?fxly=search_v2".format(id)
         names = ['education', 'height', 'car', 'income', 'house', 'weight', 'constellation', 'nationality', 'zodiac', 'blood']
@@ -80,20 +89,18 @@ class JSpider(SingleSpider):
                 return self.__deep_search(id)     
 
     def req(self, page: int, payload):
-        response = self.session.post(url = self.url, data=payload)
-        if response.status_code == 200:
-            print('成功获取数据')
-            data = response.text[11:]
+        data = super(JSpider, self).req(payload)
+        if data == None:
+            return None 
+        else:
+            data = data[11:]
             data = data[:-13]
             try:
                 data = json.loads(data)
-                return data
+                return data 
             except JSONDecodeError:
-                print('[Error] JsonDecodeError in page {}'.format(page))
-            return None
-        else:
-            print('失败获取数据')
-            sys.exit(-1)
+                print('[Error] json decode error in page {}'.format(page))
+                return None
     
     # 提取用户信息并从个人主页中拿到更加详细的个人信息
     def extarct(self, data):
@@ -124,19 +131,7 @@ class JSpider(SingleSpider):
                 info = [str(v) for v in item.values()]
                 csv_writer.writerow(info)
 
-    def run(self, page: int):
-        payload = [
-            ('sex', 'f'),
-            ('key', ''), 
-            ('stc', '23:1'),
-            ('sn', 'default'),
-            ('sv', '1'),
-            ('p', str(page)),
-            ('f', ''), 
-            ('listStyle', 'bigPhoto'),
-            ('pri_uid', '254091710'),
-            ('jsversion', 'v5')
-        ]
+    def run(self, page: int, payload: dict):
         data = self.req(page, payload)
         if data == None:
             return 
@@ -144,3 +139,24 @@ class JSpider(SingleSpider):
             flush_data = self.extarct(data)
             print(flush_data)
             self.store_csv("new_data.csv", flush_data)
+
+
+class ZSpider(SingleSpider):
+    def req(self, page, payload):
+        data = super(ZSpider, self).req(payload)
+        if data == None:
+            return None
+        else:
+            try:
+                data = json.loads(data)
+                return data
+            except JSONDecodeError:
+                print('[Error] Json decode error in page {}'.format(page))
+                return None
+
+    def run(self, page, payload):
+        super(ZSpider, self).run(page)
+        data = self.req(page, payload)
+        print(data)
+
+    
