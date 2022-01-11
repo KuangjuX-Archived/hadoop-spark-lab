31e533d357dc0212e7d3ea0d319e76b4d87b812c
KuangjuX <18630816527@163.com>
2022-01-11 14:32:33 +0800

    add reduce

--- spider/scheduler.py
+++ spider/scheduler.py
@@ -1,4 +1,5 @@
 import os
+import csv
 
 def scheduler():
     for i in range(0, 5):
@@ -8,5 +9,22 @@ def scheduler():
         filename = "data/世纪佳缘-{}.csv".format(i)
         os.system("start python ./main.py {} {} {}".format(start_page, end_page, filename))
 
+def reduce():
+    output_filename = "data/新世纪佳缘.csv"
+    input_filenames = ["data/世纪佳缘-{}.csv".format(i) for i in range(0,5)]
+    writer = open(output_filename, "a+", encoding = 'utf-8')
+    csv_writer = csv.writer(writer)
+    for filename in input_filenames:
+        reader = open(filename, "r+", encoding = 'utf-8')
+        csv_reader = csv.reader(reader)
+        for line in csv_reader:
+            csv_writer.writerow(line)
+        reader.close()
+    writer.close()
+
+    
+
+
 if __name__ == '__main__':
     scheduler()
+    reduce()
