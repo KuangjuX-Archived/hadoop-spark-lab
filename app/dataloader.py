4a40f043d39fab265ed0eb26a519b43975d785e9
BenchBeach <1779227906@qq.com>
2022-01-28 19:51:50 +0800

    edu

--- app/dataloader.py
+++ app/dataloader.py
@@ -1,3 +1,5 @@
+import math
+
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -56,7 +58,35 @@ def show_possesion(df):
     plt.show()
 
 
+def show_edu(mdf, fdf):
+    ry1 = []
+    ry2 = []
+    y1 = []
+    y2 = []
+    xticks = ['高中中专及以下', '大专', '本科', '硕士', '博士']
+    for item in xticks:
+        ry1.append(mdf[mdf['education'].str.contains(item)].shape[0])
+        ry2.append(fdf[fdf['education'].str.contains(item)].shape[0])
+    for m, f in zip(ry1, ry2):
+        y1.append(round(100 * m / sum(ry1), 2))
+        y2.append(round(100 * f / sum(ry2), 2))
+    plt.title('学历图', fontsize=13)
+    plt.bar(np.arange(len(y1)), y1, width=0.4, color='tomato', label='男性')
+    for x, y in enumerate(y1):
+        plt.text(x, y, str(y)+'%')
+
+    plt.bar(np.arange(len(y2)) + 0.4, y2, width=0.4, color='steelblue', label='女性')
+    for x, y in enumerate(y2):
+        plt.text(x + 0.4, y, str(y)+'%')
+
+    plt.xticks(np.arange(len(xticks)) + 0.4 / 2, xticks)
+    plt.ylabel('占比%')
+    plt.legend(loc='upper left')
+    plt.show()
+
+
 if __name__ == '__main__':
     plt.rcParams['font.sans-serif'] = ['SimHei']  # 用来正常显示中文标签
-    dl = DataLoader('./raw_data/female.csv')
-    show_possesion(dl.df)
+    fdl = DataLoader('./raw_data/female.csv')
+    mdl = DataLoader('./raw_data/male.csv')
+    show_edu(mdl.df, fdl.df)
